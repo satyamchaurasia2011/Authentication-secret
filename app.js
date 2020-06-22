f02f5c87fabcd994eee88054ef36083f55540e16
Satyam Chaurasia <satyamchaurasia2011@gmai.com>
2020-06-22 22:06:32 +0530

    update

--- app.js
+++ app.js
@@ -215,6 +215,12 @@ app.post("/submit", function(req, res){
 
 
 
-app.listen(3000, function(req, res){
-    console.log("running server"); 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
 });
+
